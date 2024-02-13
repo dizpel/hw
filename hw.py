701eeadfc5f30889d2f7ab848c5702fe803ec059
tatar <lavrikov.arte@mail.ru>
2024-02-13 04:32:00 +0300

    end

--- hw.py
+++ hw.py
@@ -7,3 +7,7 @@ for string in arr:
     if len(string) <= 3:
         
         new_arr.append(string)
+
+print("Новый массив строк с длиной <= 3 символам:")
+for string in new_arr:
+    print(string)
